183270c3a18ce845cb5bb75f1407bfa55598f2d1
Felipe-Oliveira <felipe.oliveira@efabrika.com.br>
2019-10-01 18:12:48 -0300

    Implementacao dos atributos, associações e construtores

--- Models/Departamento.cs
+++ Models/Departamento.cs
@@ -9,5 +9,16 @@ namespace vendasWebMvc.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public ICollection<Vendedor> Vendedores { get; set; } = new List<Vendedor>();
+
+        public Departamento()
+        {
+        }
+
+        public Departamento(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
     }
 }
