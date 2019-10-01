bf3219fed4f3b66c840a802dcb6a26cc4c42ec68
Felipe-Oliveira <felipe.oliveira@efabrika.com.br>
2019-10-01 14:52:46 -0300

    CRUD Scaffolding

---
+++ Data/vendasWebMvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace vendasWebMvc.Models
+{
+    public class vendasWebMvcContext : DbContext
+    {
+        public vendasWebMvcContext (DbContextOptions<vendasWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<vendasWebMvc.Models.Departamento> Departamento { get; set; }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using vendasWebMvc.Models;
 
 namespace vendasWebMvc
 {
@@ -33,6 +35,9 @@ namespace vendasWebMvc
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<vendasWebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("vendasWebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
